7861d06649c1da10ffe8602469f01c9101c750ef
Yimeng Zhang <zym1010@gmail.com>
2015-07-22 23:23:04 -0400

    add proto file for experiment parameters.

--- proto_files/compile_proto_files.sh
+++ proto_files/compile_proto_files.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
 protoc --java_out=../proto_classes rewarded_trial_template.proto
-
+protoc --java_out=../proto_classes import_params.proto
 # then compile Java class files.
 javac -cp ../3rdparty/protobuf-java-2.6.1.jar -d ../proto_classes -source 1.6 -target 1.6 -bootclasspath ../3rdparty/jre_16/rt.jar ../proto_classes/com/leelab/monkey_exp/*.java
